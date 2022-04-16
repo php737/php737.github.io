1c9dca3247161264354035842fe9e1e20e435360
七月与良言 <cok777@foxmail.com>
2022-04-16 15:05:27 +0800

    Update sms.js

--- sms/sms.js
+++ sms/sms.js
@@ -1,1 +1,1 @@
-{"api_url":"https://api.zhaji.tk","sms_url":"https://zha.zhaji.tk","buy_url":"https://zha.zhaji.tk","yhq_url":"http://xesxg.yhzu.cn/?r=/nine","duokai":"4"}
+{"api_url":"https://api.zhaji.tk","sms_url":"https://zha.zhaji.tk","buy_url":"https://zha.zhaji.tk","yhq_url":"http://xesxg.yhzu.cn/?r=/nine","duokai":"4","proxy":"120.42.46.226:6666"}
